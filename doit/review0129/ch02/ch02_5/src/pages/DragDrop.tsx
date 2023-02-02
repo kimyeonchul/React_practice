9bb89b6e12faca4bd3495d8370dc6ee369816d6d
kimyeonchul <yeonchul0807@nate.com>
2023-02-02 18:33:09 +0900

    CSS styling

--- doit/review0129/ch02/ch02_5/src/pages/DragDrop.tsx
+++ doit/review0129/ch02/ch02_5/src/pages/DragDrop.tsx
@@ -1,3 +1,28 @@
-export default function CopyMe() {
-  return <div>CompyMe!</div>
+import type { DragEvent } from "react"
+
+export default function DragDrop() {
+  const onDragStart = (e: DragEvent<HTMLElement>) => {
+    console.log("ONDragStart ", e.dataTransfer)
+  }
+  const onDragEnd = (e: DragEvent<HTMLElement>) => {
+    console.log("ONDragEnd ", e.dataTransfer)
+  }
+
+  const onDragOver = (e: DragEvent) => e.preventDefault()
+  const onDrop = (e: DragEvent) => {
+    e.preventDefault()
+    console.log("OnDrop", e.dataTransfer)
+  }
+
+  return (
+    <div>
+      <p>DragDrop</p>
+      <div draggable onDragStart={onDragStart} onDragEnter={onDragEnd}>
+        <h1>Drag Me</h1>
+      </div>
+      <div onDrop={onDrop} onDragOver={onDragOver}>
+        <h1>Drop Over Me</h1>
+      </div>
+    </div>
+  )
 }
