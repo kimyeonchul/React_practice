db983c46d1958455440c831112ff5973413af7fb
kimyeonchul <yeonchul0807@nate.com>
2023-02-03 16:49:53 +0900

    react_review0409까지

---
+++ dreamcoding/reviewchpater0407/src/AppProfile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Profile from "./components/Profile";
+import "./App.css";
+
+export default function AppProfile() {
+  return (
+    <div>
+      <Profile />
+      <Profile />
+      <Profile />
+    </div>
+  );
+}
