d64d66548d434af483dd343f0fb304cf8aea780b
kimyeonchul <yeonchul0807@nate.com>
2023-02-05 22:03:42 +0900

    챕터5-11 form 만들기

---
+++ dreamcoding/basic-form/src/AppForm.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+export default function AppForm() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  const [form, setForm] = useState({ name: "", email: "" });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+  return (
+    <form onSubmit={handleSubmit} action="">
+      <label htmlFor="name">이름:</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        value={form.name}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="email">이메일:</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        value={form.email}
+        onChange={handleChange}
+      />
+      <button>Submit</button>
+    </form>
+  );
+}
