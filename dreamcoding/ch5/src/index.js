ed08388d0f8c74434a0ff32afa27f844a1af9f59
kimyeonchul <yeonchul0807@nate.com>
2023-02-05 21:41:53 +0900

    ch5 commit

--- dreamcoding/ch5/src/index.js
+++ dreamcoding/ch5/src/index.js
@@ -5,11 +5,12 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import AppXY from "./AppXY";
 import AppMentors from "./AppMentors";
+import AppMentorsImmer from "./AppMentorsImmer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppMentors />
+    <AppMentorsImmer />
   </React.StrictMode>
 );
 
