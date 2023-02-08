562b9c020b9070527a9f34a3db0fa5ab1b23e3c7
kimyeonchul <yeonchul0807@nate.com>
2023-02-08 12:51:49 +0900

    다크모드 context 파일 재설정

--- dreamcoding/basic-context/src/index.js
+++ dreamcoding/basic-context/src/index.js
@@ -1,11 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import AppTheme from "./AppTheme";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import AppTheme from './AppTheme';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AppTheme />
