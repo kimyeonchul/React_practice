1b7c8b9547693e9abbb48d2bf20a0a052c42b020
kimyeonchul <yeonchul0807@nate.com>
2023-02-08 17:13:27 +0900

    todolist 오류 수정

---
+++ dreamcoding/todolist/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+import TodoList from "./components/TodoList";
+import { useState } from "react";
+
+const filters = ["all", "active", "completed"];
+
+function App() {
+  const [filter, setFilter] = useState(filters[0]);
+  return (
+    <div>
+      <Header filters={filters} filter={filter} onFilterChange={setFilter} />
+      <TodoList filter={filter} />
+    </div>
+  );
+}
+
+export default App;
