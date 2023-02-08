c9a785c1930af9bc538732fe3637547be50d853a
kimyeonchul <yeonchul0807@nate.com>
2023-02-08 13:57:15 +0900

    Styld-component 실습

--- dreamcoding/css/src/App.js
+++ dreamcoding/css/src/App.js
@@ -2,12 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import Button1 from "./Button1";
 import Button2 from "./Button2";
+import styled, { css } from "styled-components";
+import StyledComponent from "./StyledComponent";
 
 function App() {
   return (
     <>
       <Button1 />
       <Button2 />
+      <StyledComponent />
     </>
   );
 }
