a18b7a7894aef14b2137f20ce8437c468c85fa8c
kimyeonchul <yeonchul0807@nate.com>
2023-02-10 11:29:34 +0900

    React Router tutorial

---
+++ dreamcoding/router/src/App.js
@@ -0,0 +1,21 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+    errorElement: <p>Not Found Page</p>,
+  },
+  {
+    path: "/kyc",
+    element: <div>KimYeonChul!</div>,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
