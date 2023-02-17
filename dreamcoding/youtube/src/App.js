625d2bb118c1ea91ae19fb50b77c9e07ef3b8039
kimyeonchul <yeonchul0807@nate.com>
2023-02-17 19:45:35 +0900

    tanstact query 사용

--- dreamcoding/youtube/src/App.js
+++ dreamcoding/youtube/src/App.js
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import SearchHeader from "./components/SearchHeader";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryclient = new QueryClient();
 function App() {
   return (
     <>
       <SearchHeader />
-      <Outlet />
+      <QueryClientProvider client={queryclient}>
+        <Outlet />
+      </QueryClientProvider>
     </>
   );
 }
